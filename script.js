a807013d10ade214fcb8e4345bbd80677c723459
tlee07 <tlee07@risd.edu>
2019-10-28 15:44:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert ("Welcome to Sound Palette Prototype!")
+
+// document.onkeydown = function(e){
+//     e = e || window.event;
+//     var key = e.which || e.keyCode;
+//     if(key===84){
+//         example();
+//     }
+// }
